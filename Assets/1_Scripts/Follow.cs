a9f966984a021ca51f4992834d3fcd31580e8f7a
KrJunSeong <bws4115@gmail.com>
2023-06-24 01:20:30 +0900

    logging Animation Cont

--- Assets/1_Scripts/Follow.cs
+++ Assets/1_Scripts/Follow.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform target;
+    [SerializeField]
+    private bool camMove = false;
+
+    [SerializeField]
     public Vector3 OffSet;
+
+    public Transform target;
     public Vector3 rot;
     float x;
     float y;
@@ -19,14 +24,18 @@ public class Follow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // if (Input.GetKey(KeyCode.LeftAlt)) Cursor.lockState = CursorLockMode.None;
-       // else Cursor.lockState = CursorLockMode.Locked;
+        // if (Input.GetKey(KeyCode.LeftAlt)) Cursor.lockState = CursorLockMode.None;
+        // else Cursor.lockState = CursorLockMode.Locked;
 
-        transform.position = target.position + OffSet;
+        if (camMove) MoveCamera();
+        else transform.position = target.position + OffSet;
+    }
+    void MoveCamera()
+    {
         rot.x += Input.GetAxisRaw("Mouse X");
         rot.y += Input.GetAxisRaw("Mouse Y");
         rot.z = 0.0f;
 
-        transform.rotation = Quaternion.Euler(-rot.y, rot.x , 0);
+        transform.rotation = Quaternion.Euler(-rot.y, rot.x, 0);
     }
 }

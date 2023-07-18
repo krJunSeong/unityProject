3c2cd81880c5330d4d8dbd6d76ec406fac186d19
Junseong <86392396+krJunSeong@users.noreply.github.com>
2023-07-18 15:50:22 +0900

    인벤토리 작성중
    2

--- Assets/1_Scripts/Item.cs
+++ Assets/1_Scripts/Item.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu]
 public class Item : ScriptableObject
--- Assets/1_Scripts/Slot.cs
+++ Assets/1_Scripts/Slot.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class Slot : MonoBehaviour
 {
-    [SerializeField]
-    GameObject slotItem = null;
-    Transform t;
+    public Image slotImg = null;
     Item itemData;
 
+    public Sprite testImg = null;
+    
     void Start()
     {
         //slotItem = transform.GetChild(0).gameObject;
-        slotItem = transform.Find("slotItem").gameObject; 
         //slotItem.GetComponent<GameObject>();
+        slotImg = transform.Find("slotItem")?.GetComponent<Image>();
+
+        
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F1))
+            ChangeItemImage(itemData.itemImage);
+
+        if (Input.GetKeyDown(KeyCode.F2))
+            ChangeItemImage(testImg);
+    }
+
+    void ChangeItemImage(Sprite img)
+    {
+        slotImg.sprite = img;
+    }
+    void GetItem(Item i)
+    {
+        itemData = i;
     }
 }
 
@@ -27,10 +44,17 @@ public class Slot : MonoBehaviour
       - 자기 자신 포함해서 자식까지 찾아서 컴포넌트를 갖고 올 때 사용
       - 만약 자식을 찾고 싶은 거라면 trnasform.Getchild(int index)
                                    transform.FindChild(string str)
+
+      - 자기 자신을 제외한 자식들을 찾고 싶다면 foreach (Transform t in transform) listName.add(t);
+
   transform.Find(string)
         - 자기 자식에서 찾아서 반환.
 
   Array to List
         - List.addRange(Array);
         - List<> listName = new List(Array);
+
+ 리소스 호출 && ScriptableObject 사용할 때
+        - itemData = new Item();
+        - itemData.itemImage = Resources.Load<Sprite>("9_Assetes/RPG_inventory_icons/book");
 */

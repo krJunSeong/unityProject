ba084842503fa599b4c745a7e784a63f7c3cc013
KrJunSeong <bws4115@gmail.com>
2023-08-04 09:30:41 +0900

    commit stash
    slot

--- Assets/1_Scripts/Slot.cs
+++ Assets/1_Scripts/Slot.cs
@@ -27,13 +27,17 @@ public class Slot : MonoBehaviour
         //slotItem = transform.GetChild(0).gameObject;
         //slotItem.GetComponent<GameObject>();
         slotImg = transform.Find("slotItem")?.GetComponent<Image>();
+
         LoadItemTextData();
+
+        itemData = Resources.Load<Item>("testFolder/bookData");
+        testImg = itemData.itemImage;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F1))
-            ChangeItemImage(itemData.itemImage);
+            ChangeItemImage(itemData?.itemImage);
 
         if (Input.GetKeyDown(KeyCode.F2))
             ChangeItemImage(testImg);

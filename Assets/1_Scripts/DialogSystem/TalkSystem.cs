4fb12bd18fb6ded7f64e7e3a20d3a3fd7109a431
KrJunSeong <bws4115@gmail.com>
2024-02-20 12:06:54 +0900

    다이얼로그 시스템 수정?

--- Assets/1_Scripts/DialogSystem/TalkSystem.cs
+++ Assets/1_Scripts/DialogSystem/TalkSystem.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class TalkSystem : MonoBehaviour
 {
     public static TalkSystem Instance { get; private set; }
+
+    [SerializeField] GameObject uiTalkPanel;
+    [SerializeField] Image playerImg;
+    [SerializeField] Image cpuImg;
+
+    [SerializeField] Text talkerName;
+    [SerializeField] Text script;
+    [SerializeField] DepthOfField depthOfField;
+
+    public bool isTalking = false; // 대화중인가?
+    int scriptLineNum = 0;
 
     void Awake()
     {
@@ -19,27 +31,14 @@ public class TalkSystem : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    [SerializeField] GameObject uiTalkPanel;
-    [SerializeField] Image playerImg;
-    [SerializeField] Image cpuImg;
-
-    [SerializeField] Text talkerName;
-    [SerializeField] Text script;
-
-
-    public bool isTalking = false; // 대화중인가?
-    bool isNext = false;    // 다음키 입력대기
-    bool isTalkEnd = false; // 대화가 마지막까지 갔는가?
-    int scriptLineNum = 0;
+    private void Start()
+    {
+        depthOfField = GameObject.Find("Main Camera").GetComponent<PostProcessVolume>().profile.GetSetting<DepthOfField>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) Talk(0);
-    }
-
-    void SetUI(bool b)
-    {
-        uiTalkPanel.SetActive(b);
     }
     
     void Talk(int chapter)
@@ -51,32 +50,14 @@ public class TalkSystem : MonoBehaviour
         {
             // 1-1. 스크립트가 끝났거나, 스크립트가 없을 경우 UI 없애준다.
             // (아직 안 함) 1-2. 볼륨 프로세서에서 블룸 Off 한다
-            SetUI(false);
+            ActivePannel(false);
             scriptLineNum = 0;
             return;
         }
 
         // 2. 스크립트가 있다면 출력한다.
-        script.text = "";
-        talkerName.text = chData.name;
-        StartCoroutine(TypeWriter(chData.script.Replace("_", ",")));
-        //script.text = chData.script.Replace("*", ",");
-        scriptLineNum++;
-
-        if(talkerName.text == "코하쿠")
-        {
-            // 주인공이라면 주인공 이미지 변경
-            playerImg.sprite = DataManager.Instance.GetCharacterTalkImage(int.Parse(chData.image));
-            playerImg.enabled = true;
-            cpuImg.enabled = false;
-        }
-        else
-        {
-            // 주인공이 아니라면 CPU 이미지 변경
-            cpuImg.sprite = DataManager.Instance.GetCharacterTalkImage(int.Parse(chData.image));
-            playerImg.enabled = false;
-            cpuImg.enabled = true;
-        }
+        ActivePannel(true);
+        ScriptControll(chData);
     }
 
     IEnumerator TypeWriter(string s)
@@ -88,5 +69,34 @@ public class TalkSystem : MonoBehaviour
 
             yield return new WaitForSeconds(0.1f);
         }
+    }
+
+    void ScriptControll(ChatData _chData)
+    {
+        StopAllCoroutines();
+        script.text = "";
+        talkerName.text = _chData.name;
+        StartCoroutine(TypeWriter(_chData.script.Replace("_", ",")));
+        scriptLineNum++;
+
+        if (talkerName.text == "코하쿠")
+        {
+            // 주인공이라면 주인공 이미지 변경
+            playerImg.sprite = DataManager.Instance.GetCharacterTalkImage(int.Parse(_chData.image));
+            playerImg.enabled = true;
+            cpuImg.enabled = false;
+        }
+        else
+        {
+            // 주인공이 아니라면 CPU 이미지 변경
+            cpuImg.sprite = DataManager.Instance.GetCharacterTalkImage(int.Parse(_chData.image));
+            playerImg.enabled = false;
+            cpuImg.enabled = true;
+        }
+    }
+    void ActivePannel(bool b)
+    {
+        uiTalkPanel.SetActive(b);
+        depthOfField.active = b;
     }
 }

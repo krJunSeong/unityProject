ebae90c4629299e9b7a54ab94a454f44be29a4e6
Junseong <86392396+krJunSeong@users.noreply.github.com>
2024-03-05 20:00:16 +0900

    사운드매니저 작업2

--- Assets/1_Scripts/Manager/SoundManager.cs
+++ Assets/1_Scripts/Manager/SoundManager.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
-{
+{ 
     public static SoundManager Instance;
-    AudioSource audioSource;
+    public enum Sfx {BUTTON, SWING1, SWING2, SWING3,
+        KOHAKU_ATTACK1, KOHAKU_ATTACK2, KOHAKU_ATTACK3, KOHAKU_DEAD,
+        KOHAKU_HIT1, KOHAKU_HIT2, KOHAKU_WINSOUND1, KOHAKU_WINSOUND2,
+        ENEMY_ATTACK, ENEMY_DEAD, ENEMY_HIT1, ENEMY_HIT2
+    };
+    public enum Bgm {COSTALTOWN };
+
+    [SerializeField] AudioClip[] bgmClips;
+    [SerializeField] AudioClip[] sfxClips;
+
+    AudioSource bgmPlayer;
+    AudioSource[] sfxPlayers;
+
+    [SerializeField] int channels;
+    [SerializeField] float bgmVolume = 0.7f;
+    [SerializeField] float sfxVolume = 0.3f;
+    int usedLastChannel;
 
     void Awake()
     {
@@ -22,23 +38,50 @@ public class SoundManager : MonoBehaviour
 
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        Init();
     }
 
-    void SetVolume(float f)
+    void Init()
     {
-        // 인수가 1 이상으로 들어올 경우 100을 기준으로 줄여준다.
-        // ex) 94.3 = 0.94
+        //  1) BGM 사운드 오브젝트 생성, 붙이기
+        //      1-1) BGM 오브젝트에 1개의 Sound Component 장착
+        //      1-2) loop True, volume, clip 작성 후 play 해준다.
+        GameObject bgmObject = new GameObject("BGM");
+        
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClips[(int)Bgm.COSTALTOWN];
+        bgmPlayer.Play();
 
-        /*
-        if (f > 1.0f) f = ((f / 100.0f) + (f % 100.0f));
+        //  2) SFX 사운드 오브젝트 생성
+        //      2-1) soundManager 아래로 오브젝트들을 붙여준다
+        //      2-2) 오디오소스 channel 만큼 붙여준다.
+        GameObject sfxObject = new GameObject("SFX");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
 
-        audioSource.volume = f;
-        */
+        for(int i = 0; i < channels; i++)
+        {
+            sfxPlayers[i] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[i].playOnAwake = false;
+            sfxPlayers[i].volume = sfxVolume;
+        }
     }
 
-    void SetAudioClip(AudioClip clip)
+    public void PlaySfx(Sfx sfx)
     {
+        for(int i = 0; i < sfxPlayers.Length; i++)
+        {
+            int loopIndex = (i + usedLastChannel) % sfxPlayers.Length;
 
+            if (sfxPlayers[loopIndex].isPlaying) continue;
+
+            usedLastChannel = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClips[(int)sfx];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }
     }
 }
